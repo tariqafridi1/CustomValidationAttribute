ee415fce57c423362e53c0a7785d98df033283d6
tariqafridi1 <afriditariq24@gmail.com>
2023-10-13 12:56:12 +0500

    Add project files.

---
+++ CustomValidationAttribute/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CustomValidationAttribute.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CustomValidationAttribute.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CustomValidationAttribute/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using CustomValidationAttribute.Utility;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomValidationAttribute.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+       [AllowedExtensions(new string[] { ".jpg", ".png", ".jpeg" })]
+        public IFormFile? Image { get; set; }
+    }
+}
